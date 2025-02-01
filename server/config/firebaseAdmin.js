634ccc547cd3ddadb7cc0e5e92b21e7f8fbab6a9
yoav1870 <yoav18700@gmail.com>
2025-02-01 13:34:10 +0200

    render bugfix

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -1,5 +1,6 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./service-account-key.json");
+// const serviceAccount = require("./service-account-key.json"); //for local
+const serviceAccount = require("/etc/secrets/service-account-key.json"); // for prod
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
